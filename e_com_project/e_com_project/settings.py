6e572dd283232dc70152ade8c351bc0f3ecbe85d
Althaf-444 <altmd444@gmail.com>
2025-09-26 14:47:35 +0530

    add

--- e_com_project/e_com_project/settings.py
+++ e_com_project/e_com_project/settings.py
@@ -145,5 +145,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'altmd33@gmail.com'
-EMAIL_HOST_PASSWORD = 'lxjvldxqvkdutwrf'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+# lxjvldxqvkdutwrf
